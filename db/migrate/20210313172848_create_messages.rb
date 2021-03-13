3fb1f8ec65075773d612d033f5983e8576c7094c
othaderek <otha.hernandez@gmail.com>
2021-03-13 12:34:46 -0500

    FIxed broken migrations.

--- db/migrate/20210313172848_create_messages.rb
+++ db/migrate/20210313172848_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.reference :user
-      t.reference :conversation
+      t.references :user, null: false, foreign_key: true
+      t.references :conversation, null: false, foreign_key: true
       t.string :body
 
       t.timestamps
